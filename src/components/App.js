4b39aff3274cb4ac338826ad08a9ac677cbfdd81
Karen V <karenvo@usc.edu>
2020-01-18 11:07:07 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,20 @@ import GifListContainer from '../containers/GifListContainer'
 const API_KEY = "dc6zaTOxFJmzC&";
 const URL_BASE = "https://api.giphy.com/v1/gifs/";
 
-const App = () => {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: "corgis"
+    }
+  }
+  render () {
+    return (
     <div>
         < NavBar color='black' title="Giphy Search" />
+        < GifListContainer apiKey={API_KEY} urlBase={URL_BASE} />
     </div>
-  )
+    )
+  }
 }
-
 export default App
