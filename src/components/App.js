1481b83ac61774dfa030cd184bb93b81d39f91b7
Karen V <karenvo@usc.edu>
2020-01-18 11:05:05 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import NavBar from './NavBar'
 
 // the App component should render out the GifListContainer component 
+import GifListContainer from '../containers/GifListContainer'
+
+const API_KEY = "dc6zaTOxFJmzC&";
+const URL_BASE = "https://api.giphy.com/v1/gifs/";
 
 const App = () => {
   return (
